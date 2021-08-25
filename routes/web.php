288f97e6c14f4128817a3a13eb0c4d1beaa77f09
Furkan ÜNLÜ <frknlu@outlook.com>
2021-08-25 22:08:43 +0300

    Up

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix'=>'yonetim', 'namespace'=>'Yonetim'], function () {
+    Route::get('/',function(){
+    return "Admin";
+    });
+    Route::get('/oturumac','KullaniciController@oturumac')->name('yonetim.oturumac');
+    Route::get('/anasayfa','AnasayfaController@index')->name('yonetim.anasayfa');
+});
+
 Route::get('/', 'AnasayfaController@Index')->name('anasayfa');
 
 Route::get('/kategori/{slug_kategoriadi}','KategoriController@index')->name('kategori');
